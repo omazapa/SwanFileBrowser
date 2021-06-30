614c21582839398ea3b5652eff6460a084e44523
Omar Zapata <Omar.Zapata@cern.ch>
2021-06-30 14:51:31 +0200

    added required files for icons

---
+++ src/icons.ts
@@ -0,0 +1,9 @@
+import { LabIcon } from '@jupyterlab/ui-components';
+
+import swanProjectIconStr from '../style/project.svg';
+
+export const swanProjectIcon = new LabIcon({
+  name: 'ui-components:folder-project',
+  svgstr: swanProjectIconStr
+});
+
